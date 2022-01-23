0f0a95241ab5ca2bafd87dd567660c016a94f5d6
LoicMonard <monard@intechinfo.fr>
2022-01-23 17:03:37 +0100

    feature(form): add form version
    Signed-off-by: LoicMonard <monard@intechinfo.fr>

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -6,120 +6,47 @@
         type="text" 
         name="username" 
         id="username"
-        v-model="form.username"
-        :class="getInputClass('username')"
-        @change="onInputChange($event, 'username')"
       >
       <label for="username" class="placeholder">
         <span>Enter username</span>
       </label>
-      <ul class="errors" v-if="errors.username && errors.username.length">
-        <li v-for="(error, index) in errors.username" :key="index">
-          {{ error}}
-        </li>
-      </ul>
     </div>
     <div class="input">
       <input 
         type="password" 
         name="password" 
         id="password"
-        v-model="form.password"
-        :class="getInputClass('password')"
-        @change="onInputChange($event, 'password')"
       >
       <label for="password" class="placeholder">
         <span>Enter password</span>
       </label>
-      <ul class="errors" v-if="errors.password && errors.password.length">
-        <li v-for="(error, index) in errors.password" :key="index">
-          {{ error}}
-        </li>
-
-      </ul>
     </div>
     <div class="input">
       <input 
         type="text" 
         name="email" 
         id="email"
-        v-model="form.email"
-        :class="getInputClass('email')"
-        @change="onInputChange($event, 'email')"
       >
       <label for="email" class="placeholder">
         <span>Enter email</span>
       </label>
-      <ul class="errors" v-if="errors.email && errors.email.length">
-        <li v-for="(error, index) in errors.email" :key="index">
-          {{ error}}
-        </li>
-
-      </ul>
     </div>
     <div class="actions">
-      <button @click="fillData">Fill</button>
-      <button type="submit" :disabled="!isFormValid">Create account</button>
+      <button type="submit">Create account</button>
     </div>
   </form>
 </template>
 
 <script>
-import validationMixin from '../mixins/validationMixin.js'
-
 export default {
   name: "registration",
   data: () => ({
-    form: {
-      username: '',
-      password: '',
-      email: '',
-    },
-    errors: {}
   }),
-  computed: {
-    isFormValid() {
-      return this.validateForm(this.form).formIsValid;
-    }
-  },
   methods: {
     getInputClass(inputName) {
       return this.form[inputName].length ? "filled" : "";
     },
-    onInputChange(e, inputName) {
-      const inputValue = e.target.value;
-      const inputErrors = this.validateField(inputName, inputValue);
-      if(inputErrors && inputErrors.length) {
-        this.errors[inputName] = inputErrors;
-      } else {
-        this.errors[inputName] = null;
-      }
-      this.setInputClass(e, inputName);
-    },
-    setInputClass(e, inputName) {
-      if(this.errors[inputName]) {
-        document.getElementById(inputName).classList.add("danger");
-        document.getElementById(inputName).classList.remove("valid");
-      } else {
-        document.getElementById(inputName).classList.remove("danger");
-        document.getElementById(inputName).classList.add("valid");
-      }
-    },
-    fillData(e) {
-      e.preventDefault();
-      this.form = {
-        username: "John",
-        password: "Qwerty123()/",
-        email: "john@doe.com"
-      }
-      this.$nextTick(() => {
-        Object.keys(this.form).forEach(inputName => {
-          this.setInputClass(null, inputName);
-        })
-      })
-    }
   },
-  mixins: [validationMixin]
 }
 </script>
 
@@ -157,16 +84,8 @@ form {
       &:focus, &:hover, &.filled {
         border: 2px solid var(--info-color);
       }
-      &:focus + .placeholder span, &.filled + .placeholder span {
+      &:focus + .placeholder span {
         transform: translateY(-100%);
-      }
-      &.danger {
-        border: 2px solid var(--danger-color) !important;
-        color: var(--danger-color);
-      }
-      &.valid {
-        border: 2px solid var(--success-color) !important;
-        color: var(--success-color);
       }
     }
     .placeholder {
@@ -182,58 +101,16 @@ form {
         font-size: 14px;
       }
     }
-    .errors {
-      @include flex;
-      gap: 4px;
-      list-style: none;
-      padding: 0;
-      margin: 0;
-      font-size: 12px;
-      color: var(--danger-color);
-      li {
-        animation: swipeDown .3s ease forwards
-      }
-    }
   }
-  .actions {
-    @include flex-row;
-    gap: 4px;
-    button {
-      border: none;
-      border-radius: 4px;
-      padding: 10px 12px;
-      background-color: var(--info-color);
-      color: var(--white-color);
-      cursor: pointer;
-      position: relative;
-      transition: all .3s ease;
-      &:disabled {
-        cursor: not-allowed;
-        &::after {
-          content: "";
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background-color: #fff;
-          opacity: .5;
-        }
-      }
-      &[type=submit] {
-        flex: 1;
-      }
-    }
+  button {
+    border: none;
+    border-radius: 4px;
+    padding: 10px 12px;
+    background-color: var(--info-color);
+    color: var(--white-color);
+    cursor: pointer;
+    position: relative;
+    transition: all .3s ease;
   }
 } 
-
-@keyframes swipeDown {
-  from {
-    opacity: 0;
-    tranform: translateY(-10px);
-  } to {
-    opacity: 1;
-    tranform: translateY(0px);
-  }
-}
 </style>
